5c581569010a16b0b5825b95fe4e6d9120397190
stardust6653 <stardust6653@gmail.com>
2022-06-12 02:59:31 +0900

    create-react-app
    CRA를 활용한 앱만들기
    (module과 useEffect 사용)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState, useEffect } from "react";
+
+function Hello(){
+  useEffect(() => {
+    console.log("Hi :)");
+    return () => console.log("Bye :(")
+  }, [])
+  return(
+    <h1>Hello</h1>
+  )
+}
+
+// <clean up>
+// function Hello(){
+//   function byeFn(){
+//     console.log("Bye... :(")
+//   }
+//   function hiFn(){
+//     console.log("Created :)");
+//     return byeFn;
+//   }
+//   useEffect(hiFn, [])
+//   return(
+//     <h1>Hello</h1>
+//   )
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev)
+  }
+  return(
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
-  );
+  )
 }
 
 export default App;
